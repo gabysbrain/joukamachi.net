2a4946a72bbc6c5a1f78d68a4850df6e1620f3df
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-06-20 06:52:16 +0200

    service: samba service desc

---
+++ services/samba.nix
@@ -0,0 +1,43 @@
+{ lib, pkgs, ... }:
+
+{
+  # make shares visible in win10
+  services.samba-wsdd.enable = true;
+  networking.firewall.allowedTCPPorts = [ 5357 ];
+  networking.firewall.allowedUDPPorts = [ 5357 ];
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+
+    extraConfig = ''
+      server string = drive
+      netbios name = drive
+      security = user
+      browseable = yes
+      smb encrypt = required
+
+      # only allow local network hosts
+      hosts allow = 10.0.0. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+
+      # no guests
+      guest account = nobody
+      map to guest = bad user
+    '';
+
+    shares = {
+      # share home dirs
+      homes = {
+        browseable = "no";
+        "read only" = "no";
+        "guest ok" = "no";
+      };
+
+      # TODO: share video dir
+    };
+  };
+
+  # TODO: avahi config
+}
+
