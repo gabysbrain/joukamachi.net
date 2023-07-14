d71885b915833b7c3d898493f726dc19ffa1fbe5
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-07-14 16:38:15 +0200

    immich: enable photos service

--- services/photos.nix
+++ services/photos.nix
@@ -5,11 +5,25 @@
     ../pkgs/immich-service.nix
   ];
 
-  virtualisation.docker.enable = true;
+  #virtualisation.docker.enable = true;
 
   services.immich = {
     enable = true;
-    dataDir = /tmp/photos;
+    port = 2283;
+    dataDir = "/home-movies/";
+    dbHostname = "db.joukamachi.net";
+  };
+  #users.users.immich = {
+    #isSystemUser = true;
+    #home = "/home-movies";
+    #group = "immich";
+  #};
+
+  services.postgresql = {
+    ensureDatabases = [ "immich" ];
+    ensureUsers = [ {
+      name = "immich";
+    } ];
   };
 }
 
