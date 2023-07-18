f171a6aa6c562c7afe4b4b510d2b35e3a5c921d5
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-07-18 14:38:35 +0200

    remove photoprism mount on appe

--- apple-configuration.nix
+++ apple-configuration.nix
@@ -30,17 +30,6 @@
 
   services.openssh.enable = true;
 
-  # filesystems for protoprism
-  fileSystems."/var/lib/private/photoprism" = {
-    device = "//drive.joukamachi.net/exports/photoprism-cagla";
-    fsType = "cifs";
-    #options = let
-        ## this line prevents hanging on network split
-        #automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      #in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-  };
-
   # Nix
   boot.tmp.cleanOnBoot = true;
 
