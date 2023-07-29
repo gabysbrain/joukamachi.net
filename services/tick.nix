41acc7af2efff59e67ab12a507fa090ffd61bf52
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-07-29 17:20:56 +0200

    service: telegraf metrics

--- services/tick.nix
+++ services/tick.nix
@@ -12,4 +12,30 @@
 
   networking.firewall.allowedTCPPorts = [ 8086 ];
 
+  age.secrets.telegraf_token.file = ../secrets/influxdb_telegraf_token.age;
+  services.telegraf = {
+    enable = true;
+    environmentFiles = [
+      # influx pw
+      config.age.secrets.telegraf_token.path
+    ];
+    extraConfig = {
+      inputs = {
+        system = {};
+        mem = {};
+        systemd_units = {};
+        swap = {};
+        diskio = {};
+      };
+      outputs = {
+        influxdb_v2 = {
+          urls = [ "http://localhost:8086" ];
+          organization = "poodlehouse";
+          bucket = "telegraf";
+          token = "$INFLUX_TOKEN";
+          #urls = [ config.services.influxdb.http.bind-address ];
+        };
+      };
+    };
+  };
 }
