4dbcd7ef813512511fc2b14e50a2825b84434315
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-08-01 07:19:16 +0200

    telegraf: snmp monitoring

--- services/tick.nix
+++ services/tick.nix
@@ -1,5 +1,7 @@
 { config, services, pkgs, ... }:
 
+let networkGear = [ "10.0.0.1" "10.0.0.2" "10.0.0.3" ];
+in
 {
   # influxdb for timeseries data
   services.influxdb2 = {
@@ -26,6 +28,40 @@
         systemd_units = {};
         swap = {};
         diskio = {};
+
+        snmp = {
+          agents = networkGear;
+          version = 2;
+          community = "public";
+
+          field = [
+            { oid =  "SNMPv2-MIB::sysName.0"; name = "host"; is_tag = true; }
+            { oid = "DISMAN-EVENT-MIB::sysUpTimeInstance"; name = "uptime"; }
+          ];
+          table = [
+            # IF-MIB::ifTable contains counters on input and output traffic as well as errors and discards.
+            { oid = "IF-MIB::ifTable"; name = "interface"; inherit_tags = [ "host" ]; field = [
+                # Interface tag - used to identify interface in metrics database
+                { oid = "IF-MIB::ifDescr"; name = "ifDescr"; is_tag = true; }
+              ];
+            }
+
+
+            # IF-MIB::ifXTable contains newer High Capacity (HC) counters that do not overflow as fast for a few of the ifTable counters
+            { oid = "IF-MIB::ifXTable"; name = "interface"; inherit_tags = [ "host" ]; field = [
+                # Interface tag - used to identify interface in metrics database
+                { oid = "IF-MIB::ifDescr"; name = "ifDescr"; is_tag = true; }
+              ];
+            }
+
+            # EtherLike-MIB::dot3StatsTable contains detailed ethernet-level information about what kind of errors have been logged on an interface (such as FCS error, frame too long, etc)
+            { oid = "EtherLike-MIB::dot3StatsTable"; name = "interface"; inherit_tags = [ "host" ]; field = [
+                # Interface tag - used to identify interface in metrics database
+                { oid = "IF-MIB::ifDescr"; name = "ifDescr"; is_tag = true; }
+              ];
+            }
+          ];
+        };
       };
       outputs = {
         influxdb_v2 = {
@@ -38,4 +74,6 @@
       };
     };
   };
+  # need the snmp stuff for snmp translating
+  systemd.services.telegraf.path = [ pkgs.net-snmp ];
 }
