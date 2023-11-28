b350b8d0851092310dca2e7291f63023bfd914f5
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-11-28 22:15:56 +0100

    service: deluge

--- services/media.nix
+++ services/media.nix
@@ -6,4 +6,14 @@
     group = "jellyfin";
   };
 
+  services.deluge = {
+    enable = true;
+    config = {
+      max_upload_speed = "0";
+      #share_ratio_limit = "0.0";
+    };
+    web.enable = true;
+    group = "jellyfin";
+  };
+
 }
