8700bc12c2b9c1c967c775e3b9510f253e3b9d29
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-01-29 16:28:01 +0100

    package: pgvecto.rs 1.11.1
    needed by immich >1.91

---
+++ pkgs/pgvecto-rs.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  dpkg,
+  postgresql,
+}:
+# orig from: https://github.com/KenMacD/etc-nixos/blob/main/pkgs/pgvecto-rs.nix
+let
+  versionHashes = {
+    "14" = "sha256-8YRC1Cd9i0BGUJwLmUoPVshdD4nN66VV3p48ziy3ZbA=";
+    "15" = "sha256-IVx/LgRnGyvBRYvrrJatd7yboWEoSYSJogLaH5N/wPA=";
+    "16" = "sha256-L+57VRFv4rIEjvqExFvU5C9XI7l0zWj9pkKvNE5DP+k=";
+  };
+  major = lib.versions.major postgresql.version;
+in
+  stdenv.mkDerivation rec {
+    pname = "pgvecto-rs";
+    version = "0.1.11";
+
+    nativeBuildInputs = [dpkg];
+
+    src = fetchurl {
+      url = "https://github.com/tensorchord/pgvecto.rs/releases/download/v${version}/vectors-pg${major}-v${version}-x86_64-unknown-linux-gnu.deb";
+      hash = versionHashes."${major}";
+    };
+
+    unpackCmd = "dpkg -x $src source";
+
+    dontBuild = true;
+    dontStrip = true;
+
+    installPhase = ''
+      install -D -t $out/lib usr/lib/postgresql/${major}/lib/*.so
+      install -D -t $out/share/postgresql/extension usr/share/postgresql/${major}/extension/*.sql
+      install -D -t $out/share/postgresql/extension usr/share/postgresql/${major}/extension/*.control
+    '';
+
+    meta = with lib; {
+      description = "Scalable Vector database plugin for Postgres, written in Rust, specifically designed for LLM";
+      homepage = "https://github.com/tensorchord/pgvecto.rs";
+      license = licenses.asl20;
+      maintainers = with maintainers; [];
+    };
+  }
