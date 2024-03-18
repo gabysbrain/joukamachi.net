658117cac41589beb752db5edaa4a775c2891dc2
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-03-18 22:26:12 +0100

    dns: upstream forward server working finally

--- services/dns.nix
+++ services/dns.nix
@@ -11,13 +11,29 @@
       blocklist.enable = true;
       settings = {
         server = {
+          #verbosity = "5";
           interface = [ "0.0.0.0" ];
-          access-control = [ "10.0.0.0/8 allow" ];
+          access-control = [ "127.0.0.0/8 allow" "10.0.0.0/24 allow" ];
+          tls-upstream = "yes";
+          tls-cert-bundle = "/etc/pki/tls/certs/ca-bundle.crt";
+          # my ISP seems to block these root DNS requests
+          #root-hints = "${pkgs.dns-root-data}/root.key";
+          #auto-trust-anchor-file = "/var/lib/unbound/root.key";
+          domain-insecure = [ "joukamachi.net" ];
+          root-hints = "${root-hints}";
+          #local-zone = ''"joukamachi.net." static'';
+          #local-data = [
+            #''"kura.joukamachi.net" IN A 10.0.0.50''
+            #''"db.joukamachi.net" IN CNAME kura.joukamachi.net''
+          #];
         };
         forward-zone = [
           {
             name = ".";
-            forward-addr = [ "10.0.0.1" "8.8.8.8" ];
+            #forward-addr = [ "10.0.0.1" ];
+            #forward-addr = [ "1.1.1.1" "8.8.8.8" ];
+            #forward-addr = [ "10.0.0.1" "8.8.8.8" ];
+            forward-addr = [ "8.8.8.8@853#dns.google" ];
             forward-tls-upstream = "yes";
           }
         ];
