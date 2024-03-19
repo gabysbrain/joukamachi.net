22510b1dbe5a8807d3cf0ac69e164c46a8cd3cb6
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-03-19 20:46:13 +0100

    fixup! dns: upstream forward server working finally

--- services/dns.nix
+++ services/dns.nix
@@ -1,8 +1,6 @@
 # from https://github.com/mirosval/unbound-blocklist/tree/main
 { config, lib, pkgs, ... }:
 
-let root-hints = pkgs.writeText "root.hints" (builtins.readFile ../named.root);
-in
 {
   config = {
     networking.firewall.allowedUDPPorts = [ 53 ];
@@ -18,13 +16,15 @@ in
           access-control = [ "127.0.0.0/8 allow" "10.0.0.0/24 allow" ];
           tls-upstream = "yes";
           tls-cert-bundle = "/etc/pki/tls/certs/ca-bundle.crt";
+          # my ISP seems to block these root DNS requests
+          #root-hints = "${pkgs.dns-root-data}/root.key";
+          #auto-trust-anchor-file = "/var/lib/unbound/root.key";
           domain-insecure = [ "joukamachi.net" ];
           #local-zone = ''"joukamachi.net." static'';
           #local-data = [
             #''"kura.joukamachi.net" IN A 10.0.0.50''
             #''"db.joukamachi.net" IN CNAME kura.joukamachi.net''
           #];
-          root-hints = builtins.toString root-hints;
         };
         forward-zone = [
           {
