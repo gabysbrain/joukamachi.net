2f9dcb92094ad5781e28cb2e041143e0540916ce
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-03-29 07:53:03 +0100

    snapcast: snap player service

---
+++ services/snapplayer.nix
@@ -0,0 +1,30 @@
+# from https://github.com/mirosval/unbound-blocklist/tree/main
+{ config, lib, pkgs, ... }:
+
+{
+  #security.rtkit.enable = true;
+  #services.pipewire = {
+    #enable = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
+    #pulse.enable = true;
+    ## If you want to use JACK applications, uncomment this
+    ##jack.enable = true;
+  #};
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  systemd.services.snapplayer = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sound.target" ];
+    #wantedBy = [
+      #"pipewire.service"
+    #];
+    #after = [
+      #"pipewire.service"
+    #];
+    serviceConfig = {
+      ExecStart = "${pkgs.snapcast}/bin/snapclient -s 5 -h snapserver.joukamachi.net";
+    };
+  };
+}
