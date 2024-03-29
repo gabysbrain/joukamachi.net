88caeca286b64412d321d27d009939b5624eef54
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-03-29 07:51:18 +0100

    mopidy: setup mpd server

--- services/mopidy.nix
+++ services/mopidy.nix
@@ -2,7 +2,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [ 6680 ];
+  networking.firewall.allowedTCPPorts = [ 6600 6680 ];
 
   age.secrets.mopidy-jellyfin-conf = {
     file = ../secrets/mopidy-jellyfin-conf.age;
@@ -12,6 +12,7 @@
     enable = true;
     extensionPackages = [
       pkgs.mopidy-iris
+      pkgs.mopidy-mpd
       pkgs.mopidy-tidal
       pkgs.mopidy-jellyfin
     ];
@@ -22,6 +23,13 @@
       port = 6680
       allowed_origins = ttw.music.joukamachi.net
       default_app = iris
+
+      [mpd]
+      enabled = true
+      hostname = 0.0.0.0
+
+      [audio]
+      output = audioresample ! audioconvert ! audio/x-raw,rate=48000,channels=2,format=S16LE ! wavenc ! filesink location=/run/snapserver/pipewire
     '';
     extraConfigFiles = [ config.age.secrets.mopidy-jellyfin-conf.path ];
   };
