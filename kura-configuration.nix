9a9da75634df8a4e55ab2cc3b95c6a0d28b8be1c
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-04-11 12:12:35 +0200

    kura: monitor zfs metrics

--- kura-configuration.nix
+++ kura-configuration.nix
@@ -98,6 +98,11 @@
     options = [ "bind" ];
   };
 
+  # Monitor zfs
+  services.telegraf.extraConfig.inputs.zfs = {
+    poolMetrics = true;
+  };
+
   networking.hostName = "kura"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
