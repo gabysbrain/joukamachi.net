de5ccae97d5793c1c8ca376cba3d228614446b6c
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-05-03 10:40:30 +0200

    grocy: need dns-based acme config

--- services/grocy.nix
+++ services/grocy.nix
@@ -9,5 +9,13 @@
     dataDir = "/var/lib/grocy";
     settings.currency = "EUR";
   };
+
+  services.nginx = {
+    # the grocy service has its own virtualhost spec
+    # but need to set this up for dns-based ACME
+    virtualHosts."grocy.joukamachi.net" = {
+      acmeRoot = null;
+    };
+  };
 }
 
