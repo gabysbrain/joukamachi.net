3d6ecfe455d25040ddb8caa13955bd1e8a917434
Thomas Torsney-Weir <torsneyt@gmail.com>
2025-09-29 18:07:34 +0200

    feat: remove deploy-rs stuff

--- flake.nix
+++ flake.nix
@@ -10,8 +10,6 @@
     ];
   };
 
-  # For accessing `deploy-rs`'s utility Nix functions
-
   inputs = {
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
@@ -20,9 +18,6 @@
     #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
 
     # secrets, shhhh
     agenix.url = "github:ryantm/agenix";
@@ -36,7 +31,7 @@
     };
   };
 
-  outputs = { self, devshell, flake-utils, nixos-hardware, nixpkgs, deploy-rs, agenix, adblock-unbound, ... }: {
+  outputs = { self, devshell, flake-utils, nixos-hardware, nixpkgs, agenix, adblock-unbound, ... }: {
     nixosConfigurations.kura = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ 
@@ -109,75 +104,6 @@
         adblock-unbound.nixosModules.default
       ];
     };
-
-    deploy = {
-      sshUser = "deploy";
-      sshOpts = [ "-i" "~/keys/id_deploy" ];
-      user = "root";
-      #autoRollback = false;
-      #magicRollback = false;
-
-      keepResult = true;
-
-      nodes = {
-
-        kura = {
-          hostname = "kura.joukamachi.net";
-
-          # base profile for the system
-          profiles.system = {
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.kura;
-            fastConnection = true;
-          };
-
-          # here we can list additonal service profiles
-          # TODO: learn more about these and use them!
-        };
-
-        apple = {
-          hostname = "apple.joukamachi.net";
-
-          # base profile for the system
-          profiles.system = {
-            path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.apple;
-            fastConnection = true;
-          };
-        };
-
-        bananacreme = {
-          hostname = "bananacreme.joukamachi.net";
-
-          # base profile for the system
-          profiles.system = {
-            path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.bananacreme;
-            fastConnection = true;
-          };
-        };
-
-        cherry = {
-          hostname = "cherry.joukamachi.net";
-
-          # base profile for the system
-          profiles.system = {
-            path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.cherry;
-            fastConnection = true;
-          };
-        };
-
-        pumpkin = {
-          hostname = "pumpkin.joukamachi.net";
-
-          # base profile for the system
-          profiles.system = {
-            path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.pumpkin;
-            fastConnection = true;
-          };
-        };
-      };
-    };
-
-    # This is highly advised, and will prevent many possible mistakes
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   } //
   # make numtide/devshell available as a package
   flake-utils.lib.eachDefaultSystem (system: {
