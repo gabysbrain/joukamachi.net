682fdb77f92498ea0a77407dad06b47c648d41b5
Thomas Torsney-Weir <torsneyt@gmail.com>
2026-01-25 17:05:10 +0100

    feat: one-off forgejo runnner config

---
+++ services/actions_runner.nix
@@ -0,0 +1,34 @@
+{ pkgs, config, ... }:
+{
+  age.secrets.forgejo-runner-token.file = ../secrets/forgejo-runner-token.age;
+
+  # podman or docker needed for vistualized runner hosts
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+    instances.default = {
+      enable = true;
+      name = "kura";
+      url = "https://code.joukamachi.net";
+      # Obtaining the path to the runner token file may differ
+      # tokenFile should be in format TOKEN=<secret>, since it's EnvironmentFile for systemd
+      tokenFile = config.age.secrets.forgejo-runner-token.path;
+      labels = [
+        "ubuntu-latest:docker://node:16-bullseye"
+        "ubuntu-22.04:docker://node:16-bullseye"
+        "ubuntu-20.04:docker://node:16-bullseye"
+        "ubuntu-18.04:docker://node:16-buster"
+        ## optionally provide native execution on the host:
+        "native:host"
+      ];
+    };
+  };
+}
