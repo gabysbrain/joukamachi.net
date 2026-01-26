cff178aa483f827898ec7ada8c80b4c6385a20ec
Thomas Torsney-Weir <torsneyt@gmail.com>
2026-01-26 13:34:05 +0100

    feat: switch to nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
@@ -31,91 +31,104 @@
     };
   };
 
-  outputs = { self, devshell, flake-utils, nixos-hardware, nixpkgs, agenix, adblock-unbound, ... }: {
-    nixosConfigurations.kura = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        ./kura-configuration.nix
-        agenix.nixosModules.default
-        adblock-unbound.nixosModules.default
-      ];
-    };
-    nixosConfigurations.apple = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        # rpi stuff
-        #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
-        }
-        # actual system stuff
-        ./apple-configuration.nix
-        agenix.nixosModules.default
-        adblock-unbound.nixosModules.default
-      ];
-    };
-    nixosConfigurations.bananacreme = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        # rpi stuff
-        #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
-        }
-        # actual system stuff
-        ./bananacreme-configuration.nix
-        agenix.nixosModules.default
-      ];
-    };
-    nixosConfigurations.cherry = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        # rpi stuff
-        nixos-hardware.nixosModules.raspberry-pi-4
-        #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
-        }
-        # actual system stuff
-        ./cherry-configuration.nix
-        agenix.nixosModules.default
-        adblock-unbound.nixosModules.default
-      ];
-    };
-    nixosConfigurations.pumpkin = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        # rpi stuff
-        #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
-        }
-        # actual system stuff
-        ./pumpkin-configuration.nix
-        agenix.nixosModules.default
-        adblock-unbound.nixosModules.default
-      ];
-    };
-  } //
-  # make numtide/devshell available as a package
-  flake-utils.lib.eachDefaultSystem (system: {
-    devShells.default = 
-      let pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ devshell.overlays.default ];
+  outputs =
+    {
+      self,
+      devshell,
+      flake-utils,
+      nixos-hardware,
+      nixpkgs,
+      agenix,
+      adblock-unbound,
+      ...
+    }:
+    {
+      nixosConfigurations.kura = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./kura-configuration.nix
+          agenix.nixosModules.default
+          adblock-unbound.nixosModules.default
+        ];
       };
-      in
-      pkgs.devshell.mkShell {
-        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+      nixosConfigurations.apple = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # rpi stuff
+          #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.hostPlatform.system = "aarch64-linux";
+            nixpkgs.buildPlatform.system = "x86_64-linux"; # If you build on x86 other wise changes this.
+          }
+          # actual system stuff
+          ./apple-configuration.nix
+          agenix.nixosModules.default
+          adblock-unbound.nixosModules.default
+        ];
       };
-  });
+      nixosConfigurations.bananacreme = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # rpi stuff
+          #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.hostPlatform.system = "aarch64-linux";
+            nixpkgs.buildPlatform.system = "x86_64-linux"; # If you build on x86 other wise changes this.
+          }
+          # actual system stuff
+          ./bananacreme-configuration.nix
+          agenix.nixosModules.default
+        ];
+      };
+      nixosConfigurations.cherry = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # rpi stuff
+          nixos-hardware.nixosModules.raspberry-pi-4
+          #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.hostPlatform.system = "aarch64-linux";
+            nixpkgs.buildPlatform.system = "x86_64-linux"; # If you build on x86 other wise changes this.
+          }
+          # actual system stuff
+          ./cherry-configuration.nix
+          agenix.nixosModules.default
+          adblock-unbound.nixosModules.default
+        ];
+      };
+      nixosConfigurations.pumpkin = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # rpi stuff
+          #"${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.hostPlatform.system = "aarch64-linux";
+            nixpkgs.buildPlatform.system = "x86_64-linux"; # If you build on x86 other wise changes this.
+          }
+          # actual system stuff
+          ./pumpkin-configuration.nix
+          agenix.nixosModules.default
+          adblock-unbound.nixosModules.default
+        ];
+      };
+    }
+    //
+      # make numtide/devshell available as a package
+      flake-utils.lib.eachDefaultSystem (system: {
+        devShells.default =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ devshell.overlays.default ];
+            };
+          in
+          pkgs.devshell.mkShell {
+            imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          };
+      });
 
 }
