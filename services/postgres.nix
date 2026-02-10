230ae0be5345c16ed2697ebe0774868b7da849cb
Thomas Torsney-Weir <torsneyt@gmail.com>
2026-02-10 22:51:20 +0100

    feat: ensure authelia pg db is created

--- services/postgres.nix
+++ services/postgres.nix
@@ -17,6 +17,18 @@
       host  sameuser all 127.0.0.1/32 scram-sha-256
       host  sameuser all 10.0.0.0/24 scram-sha-256
     '';
+
+    # TODO: ideally in auth.nix but that's on another host
+    ensureDatabases = [ "authelia" ];
+    ensureUsers = [
+      {
+        name = "authelia";
+        ensureDBOwnership = true;
+        ensureClauses = {
+          login = true;
+        };
+      }
+    ];
   };
 
   networking.firewall.allowedTCPPorts = [ 5432 ];
