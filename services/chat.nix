a7758622633b3ec04b3518a8e64fc8fe3caeb0bf
Thomas Torsney-Weir <torsneyt@gmail.com>
2026-03-02 11:37:19 +0100

    fixup! feat: oauth for tuwunel

--- services/chat.nix
+++ services/chat.nix
@@ -10,14 +10,16 @@
     settings = {
       global.allow_federation = false;
       global.server_name = "joukamachi.net";
-      global.identity_provider = {
-        brand = "Authelia";
-        client_id = "matrix";
-        client_secret_file = config.age.secrets.matrix-sso-secret.path;
-        issuer_url = "https://auth.joukamachi.net";
-        callback_url = "https://my.tld.it/_matrix/client/unstable/login/sso/callback/matrix";
-        name = "Authelia";
-      };
+      global.identity_provider = [
+        {
+          brand = "Authelia";
+          client_id = "matrix";
+          client_secret_file = config.age.secrets.matrix-sso-secret.path;
+          issuer_url = "https://auth.joukamachi.net";
+          callback_url = "https://my.tld.it/_matrix/client/unstable/login/sso/callback/matrix";
+          name = "Authelia";
+        }
+      ];
     };
   };
 }
