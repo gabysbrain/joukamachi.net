67df5dc73db207a64123f3bb06330839d55c5bc2
Thomas Torsney-Weir <torsneyt@gmail.com>
2026-05-30 08:23:26 +0200

    fix: Revert "feat: switch to nixos zigbee"
    The nixos native one kept trying to use intel nodejs rather than
    aarch64 so it was broken. This one is more complicated to run but works.
    
    This reverts commit d09e3b028a2cd5dce07642df2498cc75a4532704.

--- services/zigbee.nix
+++ services/zigbee.nix
@@ -5,6 +5,10 @@
 }:
 
 {
+  imports = [
+    ../pkgs/zigbee2mqtt.nix
+  ];
+
   services.mosquitto = {
     enable = true;
     persistence = false;
@@ -17,24 +21,23 @@
     ];
   };
 
-  services.zigbee2mqtt = {
+  services.z2m = {
     enable = true;
+    arch = "aarch64";
+    zigbeeController = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_148d7bf450a4ed1198918a582981d5c7-if00-port0";
+    #zigbeeController = "/dev/ttyUSB0";
+    mqttUrl = "mqtt://mqtt.joukamachi.net:1883";
     settings = {
-      permit_join = false;
-      data_path = "/var/lib/zigbee2mqtt";
       mqtt = {
         base_topic = "zigbee2mqtt";
-        server = "mqtt://mqtt.joukamachi.net:1883";
+        #server = "mqtt://localhost:1883";
       };
-      serial.port = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_148d7bf450a4ed1198918a582981d5c7-if00-port0";
-      frontend.port = 8080;
-      homeassistant.enabled = config.services.home-assistant.enable;
     };
   };
 
   networking.firewall = {
     allowedTCPPorts = [
-      config.services.zigbee2mqtt.settings.frontend.port
+      config.services.z2m.port
       1883
     ];
   };
